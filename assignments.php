7708daebab340af664a8573eaea40a8a2941ed70
unknown <ittociwam@hotmail.com>
2015-05-30 23:33:55 -0600

    fixing link
    Signed-off-by: unknown <ittociwam@hotmail.com>

--- assignments.php
+++ assignments.php
@@ -8,6 +8,6 @@
 <h1 class="white">Assignments</h1>
 <a class="white" href="survey/survey.php"> Click here to take a survey for the new Science/Technology Building! </a>
 <br/>
-<a class="white" href="ironman/index.php"> Show Rankings for Lazy Man Ironman and see all entries for contestants. </a>
+<a class="white" href="ironman/index.php"> Try out the Lazyman Ironman application. </a>
 
 ></html>
